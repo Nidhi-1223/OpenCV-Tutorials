90c9e25b44b49e5e6c22b3c79907dd05e45a3511
nidhi1223 <nidhiworah02@gmail.com>
2022-03-13 09:56:42 +0530

    day 3 and modular approach

--- face_detection/modular approach/detector.py
+++ face_detection/modular approach/detector.py
@@ -1,17 +1,19 @@
 import cv2
 from skimage import io
-img_path = 'face_detection/Leclerc.png'
+
+#GLOBALS
 xml_path = 'face_detection/face_train.xml'
-eyes_xml_path = 'face_detection/eye_train.xml'
+# eyes_xml_path = 'face_detection/eye_train.xml'
 face_detector = cv2.CascadeClassifier(xml_path)
-eyes_detector = cv2.CascadeClassifier(eyes_xml_path)
+# eyes_detector = cv2.CascadeClassifier(eyes_xml_path)
 
 url = 'http://192.168.43.91/capture'        #change everytime you connect to hotspot and run that arduino file
 
-while(True):
+#Function that detects faces in an image and draws a rectangle over them
+def detect():
     img = io.imread(url)
     faces = face_detector.detectMultiScale(img, scaleFactor=2)
-    eyes = eyes_detector.detectMultiScale(img, scaleFactor=1.2)
+    # eyes = eyes_detector.detectMultiScale(img, scaleFactor=1.2)
     img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     for face in faces:
         top_left_x = face[0]
@@ -27,25 +29,8 @@ while(True):
                         (bottom_right_x, bottom_right_y),       #bottom corner co-ordinates
                         (255, 0, 0),                            #rgb value of rectgangle
                         2)                                      #width of the rectangle
+    
+    return img
 
 
-    for eye in eyes:
-        top_left_x = eye[0]
-        top_left_y = eye[1]
-        width = eye[2]
-        height = eye[3]
-
-        bottom_right_x = top_left_x + width
-        bottom_right_y = top_left_y + height
-
-        cv2.rectangle(img,                                      #image or the object
-                        (top_left_x, top_left_y),               #top corner co-ordinates
-                        (bottom_right_x, bottom_right_y),       #bottom corner co-ordinates
-                        (0, 0, 225),                            #rgb value of rectgangle
-                        2)                                      #width of the rectangle
-    cv2.imshow("Faces and eyes", img)
-    key = cv2.waitKey(20)
-    if (key == ord('q')):
-        break
-
-cv2.destroyAllWindows()
+    
---
+++ face_detection/modular approach/main.py
@@ -0,0 +1,29 @@
+import detector
+import tkinter
+import PIL
+from PIL import ImageTk
+
+
+#Declaring the main application window
+app = tkinter.Tk()
+app.title("**Face Detection**")
+
+#The image level that refreshes over and over
+imageLabel = tkinter.Label(app)
+imageLabel.pack()
+
+
+#the logic to refresh the label
+def showFrame():
+    #getting image array from detector
+    img = detector.detect()     #detector file mein detect function
+    #converting image array to Pillow image object
+    frame = PIL.Image.fromarray(img)
+    #creating tkinter image object and changing label's image
+    imgtkinter = ImageTk.PhotoImage(frame)
+    imageLabel.imgtk = imgtkinter
+    imageLabel.configure(image=imgtkinter)
+    imageLabel.after(150, showFrame)
+
+showFrame()
+app.mainloop()
